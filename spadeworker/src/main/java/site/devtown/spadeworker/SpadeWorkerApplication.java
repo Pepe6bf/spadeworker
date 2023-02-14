66d230edaa6d035f61cba27c74068b7aa64ec9a1
pepe6bf <pepe6bf@gmail.com>
2023-02-14 14:25:53 +0900

    Project Init

---
+++ spadeworker/src/main/java/site/devtown/spadeworker/SpadeWorkerApplication.java
@@ -0,0 +1,13 @@
+package site.devtown.spadeworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpadeWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpadeWorkerApplication.class, args);
+	}
+
+}
