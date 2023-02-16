8b763a252d2e87103538940428c7d44d1ddb8d8d
pepe6bf <pepe6bf@gmail.com>
2023-02-16 20:01:51 +0900

    feat: #17 사용자 및 사용자 권한 Entity Class 개발
    사용자와 사용자 권한을 표현하기 필요한 Entity Class를 개발하였다.
    
    * User : 사용자 테이블을 표현하는 엔티티
    * Role : 권한 테이블을 표현하는 엔티티
    * UserRole : User와 Role 테이블 간 N:M 관계를 표현하기 위한 중간 테이블
    
    ERD-Reference : https://www.erdcloud.com/d/CEgmYcGbGYA7m2gfN

---
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/user/model/entity/UserRole.java
@@ -0,0 +1,33 @@
+package site.devtown.spadeworker.domain.user.model.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.devtown.spadeworker.global.config.audit.BaseTimeEntity;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class UserRole extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Role role;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    private UserRole(Role role, User user) {
+        this.role = role;
+        this.user = user;
+    }
+
+    public static UserRole of(Role role, User user) {
+        return new UserRole(role, user);
+    }
+}
