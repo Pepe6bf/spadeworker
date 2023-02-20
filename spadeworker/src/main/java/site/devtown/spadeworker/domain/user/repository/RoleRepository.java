034608ef3e110072bc11b2f908e179ef7487ea5d
pepe6bf <pepe6bf@gmail.com>
2023-02-20 20:45:18 +0900

    feat: #18 OAuth2 Social Login을 수행하는 메인 로직 작성
    OAuth2 Social Login을 진행하는 메인 로직이 담긴 CustomOAuth2UserService를 개발하였다.
    
    * Social 로그인을 시도하여 로그인에 성공하고, 회원이 아닐경우 회원 가입을 진행 후 로그인
    * Social 로그인을 시도하여 로그인에 성공하고, 이미 기존 회원일 경우 로그인만 진행
    
    추가로 Service 로직을 작성하기 위해 필요한 Entity Class를 조회하기 위해 아래 로직들을 각각의 Repository Interface에 추가해주었다.
    
    * UserRepository : 사용자의 개인 id를 기준으로 User Entity 조회
    * RoleRepository : 사용자의 권한을 명시한 Enum Class를 기준으로 Role Entity 조회
    * UserRoleRepository : 특정 사용자 Entity Class를 기준으로 해당 사용자의 모든 권한 리스트를 조회

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/user/repository/RoleRepository.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/user/repository/RoleRepository.java
@@ -1,7 +1,12 @@
 package site.devtown.spadeworker.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.devtown.spadeworker.domain.user.model.constant.UserRoleType;
 import site.devtown.spadeworker.domain.user.model.entity.Role;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByRoleType(UserRoleType roleType);
 }
