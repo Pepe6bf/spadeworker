4fa5af028db0b6ed74225870714af52bbc11c310
pepe6bf <pepe6bf@gmail.com>
2023-02-24 20:55:07 +0900

    feat: #26 TokenAuthenticationFilter 검증 케이스 추가 및 EntryPoint에 해당 변경사항을 위한 코드 수정 반영
    - 토큰 재발급 기능을 위해서 token 재발급을 위한 api로 접근 시 필터를 통과할 수 있도록 로직을 수정하였다.
    - 사용자가 access token없이 리소스에 접근하는 경우를 상정하여 검증 로직을 추가해주었다.
    
    추가로 Filter에서 발생한 변경 사항을 적용하기 위해 CustomAuthenticationEntryPoint Class에 특정 상황에서 발생하는 예외를 각각 처리할 수 있는 로직을 추가해주었다.

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/auth/exception/CustomAuthenticationEntryPoint.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/auth/exception/CustomAuthenticationEntryPoint.java
@@ -9,19 +9,39 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import static site.devtown.spadeworker.domain.auth.exception.AuthExceptionCode.*;
+
 public class CustomAuthenticationEntryPoint
         implements AuthenticationEntryPoint {
+
     @Override
     public void commence(
             HttpServletRequest request,
             HttpServletResponse response,
             AuthenticationException authException
     ) throws IOException, ServletException {
-        response.setContentType("application/json");
-        response.setStatus(AuthExceptionCode.INVALID_TOKEN.getHttpStatus().value());
-        response.getWriter().write(
-                ExceptionResponse.of(AuthExceptionCode.INVALID_TOKEN)
-                        .convertJson()
-        );
+        String exception = (String) request.getAttribute("exception");
+
+        if (exception == null)
+            exception = "NO_EXCEPTION";
+
+        if (exception.equals(NO_TOKEN.getCode())) {
+            setResponse(response, NO_TOKEN);
+        } else if (exception.equals(EXPIRED_TOKEN.getCode())) {
+            setResponse(response, EXPIRED_TOKEN);
+        } else if (exception.equals(INVALID_TOKEN.getCode())) {
+            setResponse(response, INVALID_TOKEN);
+        } else if (exception.equals(AUTHENTICATION_CLIENT_EXCEPTION.getCode())) {
+            setResponse(response, AUTHENTICATION_CLIENT_EXCEPTION);
+        }
+    }
+
+    //한글 출력을 위해 getWriter() 사용
+    private void setResponse(HttpServletResponse response, AuthExceptionCode exceptionCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        ExceptionResponse errorResponse = ExceptionResponse.of(exceptionCode);
+        response.getWriter().print(errorResponse.convertJson());
     }
 }
