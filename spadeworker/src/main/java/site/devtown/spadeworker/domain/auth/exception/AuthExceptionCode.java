6dea65d3f7842b929a5617684a20af66afab552d
pepe6bf <pepe6bf@gmail.com>
2023-02-24 20:44:52 +0900

    feat: #26 AuthExceptionCode 수정 및 Error Code 추가
    AuthExceptionCode Enum Class의 내용을 수정하고 error code를 추가해주었다.

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/auth/exception/AuthExceptionCode.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/auth/exception/AuthExceptionCode.java
@@ -13,15 +13,29 @@ import static org.springframework.http.HttpStatus.*;
 public enum AuthExceptionCode
         implements ExceptionCode {
 
-    DUPLICATED_USER_EMAIL(CONFLICT, "ACC-001", "Duplicated user email"),
-    USER_NOT_FOUND(NOT_FOUND, "ACC-002", "User not found"),
-    INVALID_TOKEN_SIGNATURE(UNAUTHORIZED, "ACC-003", "Invalid token signature."),
-    INVALID_TOKEN(UNAUTHORIZED, "ACC-004", "Invalid token."),
-    EXPIRED_TOKEN(UNAUTHORIZED, "ACC-005", "Expired JWT token."),
-    UNSUPPORTED_TOKEN(UNAUTHORIZED, "ACC-006", "Unsupported JWT token."),
-    INVALID_TOKEN_COMPACT(INTERNAL_SERVER_ERROR, "ACC-007", "JWT token compact of handler are invalid."),
-    AUTHENTICATION_ERROR(UNAUTHORIZED, "ACC-008", "Authentication exception."),
-    INTERNAL_AUTHENTICATION_SERVICE_EXCEPTION(INTERNAL_SERVER_ERROR, "ACC-009", "Internal authentication service exception.");
+    /**
+     * 확인 불명 오류 코드
+     * 0 ~ 99
+     */
+    AUTHENTICATION_CLIENT_EXCEPTION(UNAUTHORIZED, "A000", "사용자 인증에 실패하였습니다."),
+    AUTHENTICATION_SERVICE_EXCEPTION(INTERNAL_SERVER_ERROR, "A001", "인증 로직이 실패하였습니다."),
+
+    /**
+     * JWT
+     * 100 ~ 199
+     */
+    INVALID_TOKEN(UNAUTHORIZED, "A100", "유효하지 않은 토큰 입니다."),
+    FAILED_GENERATE_TOKEN(INTERNAL_SERVER_ERROR, "A101", "토큰 생성에 실패하였습니다."),
+    EXPIRED_TOKEN(UNAUTHORIZED, "A102", "만료된 토큰 입니다."),
+    NOT_EXPIRED_TOKEN(UNAUTHORIZED, "A103", "만료되지 않은 토큰 입니다."),
+    INVALID_REFRESH_TOKEN(BAD_REQUEST, "A104", "유효하지 않은 refresh token 입니다."),
+    NO_TOKEN(BAD_REQUEST, "A105", "요청에 토큰이 존재하지 않습니다."),
+
+    INVALID_TOKEN_SIGNATURE(UNAUTHORIZED, "ACC-003", "유효하지 않은 토큰 시그니쳐입니다."),
+    UNSUPPORTED_TOKEN(UNAUTHORIZED, "ACC-006", "지원하지 않는 토큰입니다."),
+    INVALID_TOKEN_COMPACT(INTERNAL_SERVER_ERROR, "ACC-007", "유효하지 않는 토큰 생성입니다."),
+    AUTHENTICATION_ERROR(UNAUTHORIZED, "ACC-019", "Authentication exception."),
+    INTERNAL_AUTHENTICATION_SERVICE_EXCEPTION(INTERNAL_SERVER_ERROR, "ACC-020", "Internal authentication service exception.");
 
     private final HttpStatus httpStatus;
     private final String code;
