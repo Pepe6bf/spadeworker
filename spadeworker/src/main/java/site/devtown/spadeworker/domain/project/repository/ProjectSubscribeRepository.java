17d039df5d0f35b3b0fb8f8b0ad26ac6c46c89dc
pepe6bf <pepe6bf@gmail.com>
2023-03-05 20:36:13 +0900

    feat: #47 프로젝트 구독 및 취소 기능 개발
    특정 프로젝트에 구독 및 취소할 수 있는 기능을 개발하였다.

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/project/repository/ProjectSubscribeRepository.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/project/repository/ProjectSubscribeRepository.java
@@ -1,8 +1,16 @@
 package site.devtown.spadeworker.domain.project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.devtown.spadeworker.domain.project.entity.Project;
 import site.devtown.spadeworker.domain.project.entity.ProjectSubscribe;
+import site.devtown.spadeworker.domain.user.model.entity.User;
+
+import java.util.Optional;
 
 public interface ProjectSubscribeRepository
         extends JpaRepository<ProjectSubscribe, Long> {
+
+    Boolean existsByProjectAndSubscriber(Project project, User subscriber);
+
+    Optional<ProjectSubscribe> findByProjectAndSubscriber(Project project, User subscriber);
 }
