2fa51deaef1709401bbd48e37e01abb3a9aa629e
pepe6bf <pepe6bf@gmail.com>
2023-03-05 19:39:53 +0900

    exception: #47 기존 Project Like 관련 예외 수정
    기존 Project Like에 관련된 Custom Exception의 에러메세지를 정의하는 로직을 문자열 대신 enum으로 대체해주었다.

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/project/exception/ProjectDuplicateLikeException.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/project/exception/ProjectDuplicateLikeException.java
@@ -1,7 +1,9 @@
 package site.devtown.spadeworker.domain.project.exception;
 
+import static site.devtown.spadeworker.domain.project.exception.ProjectExceptionCode.*;
+
 public class ProjectDuplicateLikeException extends RuntimeException {
     public ProjectDuplicateLikeException() {
-        super("프로젝트에 이미 해당 사용자의 좋아요가 존재합니다.");
+        super(PROJECT_DUPLICATE_LIKE.getMessage());
     }
 }
