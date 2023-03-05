6dc5cce63e6c329a4a0231740560312e0b3cbc06
pepe6bf <pepe6bf@gmail.com>
2023-03-05 14:50:47 +0900

    feat: #46 Project 좋아요 관련 예외 Class 정의 및 ProjectExceptionCode에 Code 추가
    다음과 같은 Project 좋아요 시스템에 관련된 예외를 정의해주었다.
    
    - 좋아요 등록 요청이 왔을때 이미 프로젝트에 요청자의 좋아요가 등록되어 있을 경우
    - 좋아요 취소 요청이 왔을때 프로젝트에 요청자의 좋아요가 없을 경우

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/project/exception/ProjectExceptionCode.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/project/exception/ProjectExceptionCode.java
@@ -13,7 +13,9 @@ public enum ProjectExceptionCode
         implements ExceptionCode {
 
     PROJECT_NOT_FOUND(NOT_FOUND, "PJ-C-001", "존재하지 않는 프로젝트입니다."),
-    INVALID_PROJECT_OWNER(FORBIDDEN, "PJ-C-002", "프로젝트를 처리할 권한이 없습니다.");
+    INVALID_PROJECT_OWNER(FORBIDDEN, "PJ-C-002", "프로젝트를 처리할 권한이 없습니다."),
+    PROJECT_DUPLICATE_LIKE(BAD_REQUEST, "PJ-C-003", "프로젝트에 이미 해당 사용자의 좋아요가 존재합니다."),
+    PROJECT_LIKE_NOT_FOUND(NOT_FOUND, "PJ-C-004", "프로젝트에 해당 사용자의 좋아요가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
