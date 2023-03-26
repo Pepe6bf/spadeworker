5dc2050ade6d99590819f7203724735ec536400d
pepe6bf <pepe6bf@gmail.com>
2023-03-26 21:39:22 +0900

    refactor: #66 ThumbnailImageValidate 수정
    - 불필요한 @Component 어노테이션 제거
    - annotation target 수정
    - 썸네일 & 컨테츠 이미지 검증을 분리하기 위해 내부 코드를 수정

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/file/validation/ThumbnailImageValidator.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/file/validation/ThumbnailImageValidator.java
@@ -1,7 +1,6 @@
 package site.devtown.spadeworker.domain.file.validation;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import site.devtown.spadeworker.global.util.ImageUtil;
 
@@ -9,37 +8,36 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.List;
 
-@Component
 public class ThumbnailImageValidator
         implements ConstraintValidator<ThumbnailImageValidate, MultipartFile> {
 
     @Value("${image.allow-extensions}")
     private List<String> allowImageExtensions;
 
-    @Value("${image.project-thumbnail-image.limit-size}")
+    @Value("${image.limit-size}")
     private long limitImageSize;
 
     @Override
     public boolean isValid(
-            MultipartFile uploadImage,
+            MultipartFile requestImage,
             ConstraintValidatorContext context
     ) {
 
-        // 프로필 썸네일이 Null 인지 체크
-        if (uploadImage == null) {
+        // 요청된 이미지 값이 Null 인지 체크
+        if (requestImage == null) {
             context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate("프로필 썸네일은 필수 값 입니다.")
+            context.buildConstraintViolationWithTemplate("썸네일 이미지 값은 필수 입니다.")
                     .addConstraintViolation();
             return false;
         }
 
         // 입력 값으로 넘어온 이미지가 빈 값일 경우 기본 이미지를 사용한다고 판단
-        if (uploadImage.isEmpty()) {
+        if (requestImage.isEmpty()) {
             return true;
         }
 
         // 이미지 확장자 검증
-        if (!validateImageExtension(ImageUtil.getImageExtension(uploadImage.getOriginalFilename()))) {
+        if (!validateImageExtension(ImageUtil.getImageExtension(requestImage.getOriginalFilename()))) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("유효하지 않은 이미지 확장자 입니다.")
                     .addConstraintViolation();
@@ -47,7 +45,7 @@ public class ThumbnailImageValidator
         }
 
         // 이미지 크기 검증
-        if (!validateImageSize(uploadImage.getSize())) {
+        if (!validateImageSize(requestImage.getSize())) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate("유효하지 않은 이미지 크기 입니다.")
                     .addConstraintViolation();
