18f2f4d6c58d970bf7ab398e5156fd712cb05101
pepe6bf <pepe6bf@gmail.com>
2023-03-28 16:03:07 +0900

    feat: #66 임시 게시글 기본 기능 개발
    임시 게시글 도메인의 다음과 같은 기본 기능을 개발하였다.
    
    - 임시 게시글 전체 조회
    - 임시 게시글 단건 조회
    - 임시 게시글 삭제

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/article/controller/ArticleController.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/article/controller/ArticleController.java
@@ -4,10 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import site.devtown.spadeworker.domain.article.dto.SaveTempArticleRequest;
 import site.devtown.spadeworker.domain.article.dto.SaveTempArticleResponse;
+import site.devtown.spadeworker.domain.article.dto.TempArticleDto;
 import site.devtown.spadeworker.domain.article.service.ArticleService;
 import site.devtown.spadeworker.domain.file.dto.UploadContentImageRequest;
 import site.devtown.spadeworker.domain.file.dto.UploadContentImageResponse;
 import site.devtown.spadeworker.domain.file.validation.ContentImageValidate;
+import site.devtown.spadeworker.global.response.CommonResult;
+import site.devtown.spadeworker.global.response.ListResult;
 import site.devtown.spadeworker.global.response.ResponseService;
 import site.devtown.spadeworker.global.response.SingleResult;
 
@@ -59,6 +62,50 @@ public class ArticleController {
                         tempArticleId,
                         request.image()
                 )
+        );
+    }
+
+    /**
+     * 임시 게시글 전체 조회 API
+     */
+    @GetMapping("/temp-articles")
+    public ListResult<TempArticleDto> getTempArticles() {
+
+        return responseService.getListResult(
+                OK.value(),
+                "성공적으로 임시 게시글이 조회되었습니다.",
+                articleService.getTempArticles()
+        );
+    }
+
+    /**
+     * 임시 게시글 단건 조회 API
+     */
+    @GetMapping("/temp-articles/{temp-article-id}")
+    public SingleResult<TempArticleDto> getTempArticle(
+            @PathVariable("temp-article-id") Long tempArticleId
+    ) {
+
+        return responseService.getSingleResult(
+                OK.value(),
+                "성공적으로 임시 게시글이 조회되었습니다.",
+                articleService.getTempArticle(tempArticleId)
+        );
+    }
+
+    /**
+     * 임시 게시글 삭제 API
+     */
+    @DeleteMapping("/temp-articles/{temp-article-id}")
+    public CommonResult deleteTempArticle(
+            @PathVariable("temp-article-id") Long tempArticleId
+    ) {
+
+        articleService.deleteTempArticle(tempArticleId);
+
+        return responseService.getSuccessResult(
+                OK.value(),
+                "성공적으로 임시 게시글이 삭제되었습니다."
         );
     }
 }
