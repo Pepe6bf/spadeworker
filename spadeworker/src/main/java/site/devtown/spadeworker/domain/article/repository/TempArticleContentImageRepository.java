c7911b52d860fae42adb2c1d4eb76b0b9e6b2f75
pepe6bf <pepe6bf@gmail.com>
2023-03-28 16:01:39 +0900

    feat: #66 임시 게시글 관련 Repository 메서드 추가
    임시 게시글 도메인 추가 기능 개발을 위해 Repository에 필요한 메서드를 추가 개발해주었다.
    
    - 특정 임시 게시글에 관련된 본문 이미지 전체 조회
    - 특정 임시 게시글에 관련된 해시태그 전체 조회
    - 특정 사용자의 임시 게시글 전체 조회

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/article/repository/TempArticleContentImageRepository.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/article/repository/TempArticleContentImageRepository.java
@@ -1,8 +1,13 @@
 package site.devtown.spadeworker.domain.article.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import site.devtown.spadeworker.domain.article.model.entity.TempArticle;
 import site.devtown.spadeworker.domain.article.model.entity.TempArticleContentImage;
+
+import java.util.List;
 
 public interface TempArticleContentImageRepository
         extends JpaRepository<TempArticleContentImage, Long> {
+
+    List<TempArticleContentImage> findAllByTempArticle(TempArticle tempArticle);
 }
