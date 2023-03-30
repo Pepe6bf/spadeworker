ad308cd1b69145a075fcd518805004ee70ed5681
pepe6bf <pepe6bf@gmail.com>
2023-03-30 14:53:08 +0900

    feat: #67 Article & TempArticle 관련 필요한 Repository 생성 및 기능 추가

--- spadeworker/src/main/java/site/devtown/spadeworker/domain/article/repository/TempArticleHashtagRepository.java
+++ spadeworker/src/main/java/site/devtown/spadeworker/domain/article/repository/TempArticleHashtagRepository.java
@@ -4,12 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import site.devtown.spadeworker.domain.article.model.entity.TempArticle;
 import site.devtown.spadeworker.domain.article.model.entity.TempArticleHashtag;
 
-import java.util.List;
 import java.util.Set;
 
 public interface TempArticleHashtagRepository
         extends JpaRepository<TempArticleHashtag, Long> {
 
     void deleteAllByTempArticle(TempArticle tempArticle);
-    List<TempArticleHashtag> findAllByTempArticle(TempArticle tempArticle);
+
+    Set<TempArticleHashtag> findAllByTempArticle(TempArticle tempArticle);
 }
